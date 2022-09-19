af37d8db6ed5a2a3ba5ce23cc63366e0a697607c
Shawn Wagner <shawnw.mobile@gmail.com>
2022-09-19 08:08:13 -0700

    SRFI-190: Switch to syntax-parse
    Gives better detection and reporting of invalid uses of the macros.

--- srfi/190.rkt
+++ srfi/190.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 ;;; SRFI-190  Coroutine generators
 
-(require racket/contract racket/stxparam (only-in "158.rkt" make-coroutine-generator)
+(require racket/contract syntax/parse syntax/parse/define racket/stxparam
+         (only-in "158.rkt" make-coroutine-generator)
          (for-syntax racket/base))
 (module+ test (require rackunit))
 
@@ -10,19 +11,18 @@
 (define-syntax-parameter yield
   (lambda (stx) (raise-syntax-error #f "yield used outside coroutine generator" stx)))
 
-(define-syntax coroutine-generator
-  (lambda (stx)
-    (syntax-case stx ()
-      ((_ . body)
-       #'(make-coroutine-generator
-          (lambda (%yield)
-            (syntax-parameterize ([yield (make-rename-transformer #'%yield)])
-              . body)))))))
+(define-syntax-parse-rule (coroutine-generator body:expr ...+)
+  (make-coroutine-generator
+   (lambda (%yield)
+     (syntax-parameterize ([yield (make-rename-transformer #'%yield)])
+       body ...))))
 
-(define-syntax define-coroutine-generator
-  (lambda (stx)
-    (syntax-case stx ()
-      ((_ name . body) #'(define name (coroutine-generator . body))))))
+(define-syntax (define-coroutine-generator stx)
+  (syntax-parse stx
+    [(_ (name:id arg:id ...) body:expr ...+)
+     #'(define (name arg ...) (coroutine-generator body ...))]
+    [(_ name:id body:expr ...+)
+     #'(define name (coroutine-generator body ...))]))
 
 (module+ test
   (require (only-in "158.rkt" generator->list))
@@ -34,9 +34,14 @@
 
   (define-coroutine-generator (h n)
     (do ((i 0 (+ i 1)))
-        ((<= 3 i))
+        ((<= n i))
+      (yield i)))
+
+  (define-coroutine-generator hh
+    (do ((i 0 (+ i 1)))
+      ((<= 3 i))
       (yield i)))
 
   (test-equal? "coroutine-generator" (generator->list g) '(0 1 2))
-  (test-equal? "define-coroutine-generator" (generator->list (h 3)) '(0 1 2))
-  )
+  (test-equal? "define-coroutine-generator-func" (generator->list (h 3)) '(0 1 2))
+  (test-equal? "define-coroutine-generator-direct" (generator->list hh) '(0 1 2)))
