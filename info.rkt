cb0c16179dd8bbce83659db13e507f813020cec7
Shawn Wagner <shawnw.mobile@gmail.com>
2022-09-24 08:34:30 -0700

    Add build dependency on data-doc

--- info.rkt
+++ info.rkt
@@ -2,8 +2,8 @@
 (define collection 'multi)
 (define deps '("base" "typed-racket-lib" "srfi-lib" "math-lib" "data-lib"
                       ("racket" #:version "8.6")))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-typed"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-typed" "data-doc"))
 (define pkg-desc "Ports of more SRFIs to Racket")
-(define version "0.0")
+(define version "0.5")
 (define pkg-authors '(shawnw))
 (define license '(Apache-2.0 OR MIT))
--- srfi/info.rkt
+++ srfi/info.rkt
@@ -2,7 +2,7 @@
 (define collection "srfi")
 (define deps '("base" "typed-racket-lib" "srfi-lib" "math-lib" "data-lib"
                       ("racket" #:version "8.6")))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-typed"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-typed" "data-doc"))
 (define scribblings '(("scribblings/extra-srfi-libs.scrbl" ())))
 (define pkg-authors '(shawnw))
 (define license '(Apache-2.0 OR MIT))
